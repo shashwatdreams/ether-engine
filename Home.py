6f34c4c9873c212f9a69575afa99f91dce4b81ca
Shashwat Sharma <kush.shashwat@outlook.com>
2024-11-03 09:51:33 -0600

    homepage

---
+++ Home.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title("2024 Election Candidate Chatbot")
+st.subheader("Get Direct Insights from Senate and Presidential Candidates")
+
+st.write("""
+This chatbot pulls information directly from each candidate’s official website, allowing you to learn about their positions in their own words, free from third-party interpretation. Select a race and state to start exploring what each candidate stands for.
+""")
